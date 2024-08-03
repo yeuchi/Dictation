f481313d6752d44cb385b8f37f7e1c08d1d902d8
Chi Yeung <yeun0007@umn.edu>
2024-08-03 16:58:56 -0500

    release: latest api 34 per Google Play requirement

--- DictateKotlin/app/build.gradle.kts
+++ DictateKotlin/app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.ctyeung.dictatekotlin"
         minSdk = 29
         targetSdk = 34
-        versionCode = 2
-        versionName = "2.0"
+        versionCode = 7
+        versionName = "7.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
